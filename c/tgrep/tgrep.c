b1822911b37ed841cf20460580be795cabe2bda2
kyosold <kyosold@qq.com>
2020-10-13 14:21:17 +0800

    优化结尾pos点，缩小查询范围

--- c/tgrep/tgrep.c
+++ c/tgrep/tgrep.c
@@ -44,29 +44,31 @@ void copy_linetime(struct line_time_t *dst, struct line_time_t *src);
 
 unsigned char eolbyte = '\n';
 
-char *buffer;                   // Base of buffer
-size_t bufalloc;                // Allocated buffer size, counting slop.
-#define INITIAL_BUFSIZE 32768   // Initial buffer size, not counting slop.
+char *buffer;                 // Base of buffer
+size_t bufalloc;              // Allocated buffer size, counting slop.
+#define INITIAL_BUFSIZE 32768 // Initial buffer size, not counting slop.
 // char *bufbeg;       // Beginning of user-visiable stuff.
 // char *buflim;       // Limit of user-visiable stuff.
-size_t pagesize;    // alignment of memory pages
+size_t pagesize; // alignment of memory pages
 
 /*  Return VAL aligned to the next multiple of ALIGNMENT.  VAL can be
     an integer or a pointer.  Both args must be free of side effects.  */
-#define ALIGN_TO(val, alignment) \
-    ((size_t) (val) % (alignment) == 0 \
-    ? (val) \
-    : (val) + ((alignment) - (size_t) (val) % (alignment)))
+#define ALIGN_TO(val, alignment)      \
+    ((size_t)(val) % (alignment) == 0 \
+         ? (val)                      \
+         : (val) + ((alignment) - (size_t)(val) % (alignment)))
 
 int reset(int fd, char const *file)
 {
-    if (!pagesize) {
+    if (!pagesize)
+    {
         pagesize = getpagesize();
-        if (pagesize == 0 || 2 * pagesize + 1 <= pagesize) {
+        if (pagesize == 0 || 2 * pagesize + 1 <= pagesize)
+        {
             abort();
         }
-        bufalloc = ALIGN_TO (INITIAL_BUFSIZE, pagesize) + pagesize + 1;
-        buffer = (char *) xmalloc(bufalloc);
+        bufalloc = ALIGN_TO(INITIAL_BUFSIZE, pagesize) + pagesize + 1;
+        buffer = (char *)xmalloc(bufalloc);
         // printf("buffer alloc: %d\n", bufalloc);
     }
 
@@ -75,7 +77,6 @@ int reset(int fd, char const *file)
     return 1;
 }
 
-
 int get_line_time(char *line, struct line_time_t *linetime)
 {
     char month[128] = {0};
@@ -83,27 +84,39 @@ int get_line_time(char *line, struct line_time_t *linetime)
     char time[128] = {0};
     int i = 0, j = 0;
     int x = 0;
-    for (i = 0; i < strlen(line); i++) {
-        if (line[i] == ' ') {
-            if (line[i+1] == ' ') {
+    for (i = 0; i < strlen(line); i++)
+    {
+        if (line[i] == ' ')
+        {
+            if (line[i + 1] == ' ')
+            {
                 continue;
-            } else {
+            }
+            else
+            {
                 x++;
                 j = 0;
             }
             continue;
         }
 
-        if (x == 0) {
-            month[j++] = line[i]; 
+        if (x == 0)
+        {
+            month[j++] = line[i];
             month[j] = 0;
-        } else if (x == 1) {
+        }
+        else if (x == 1)
+        {
             day[j++] = line[i];
             day[j] = 0;
-        } else if (x == 2) {
+        }
+        else if (x == 2)
+        {
             time[j++] = line[i];
             time[j] = 0;
-        } else {
+        }
+        else
+        {
             break;
         }
     }
@@ -117,13 +130,20 @@ int get_line_time(char *line, struct line_time_t *linetime)
     j = 0;
     x = 0;
     char tmp[128] = {0};
-    while (time[i] != 0) {
-        if (time[i] == ':') {
-            if (x == 0) {
+    while (time[i] != 0)
+    {
+        if (time[i] == ':')
+        {
+            if (x == 0)
+            {
                 hou = atoi(tmp);
-            } else if (x == 1) {
+            }
+            else if (x == 1)
+            {
                 min = atoi(tmp);
-            } else {
+            }
+            else
+            {
                 sec = atoi(tmp);
             }
             x++;
@@ -137,8 +157,8 @@ int get_line_time(char *line, struct line_time_t *linetime)
     sec = atoi(tmp);
 
     // printf("month(%d) day(%d) hour(%d) min(%d) sec(%d)\n",
-        // mm, dd, hou, min, sec);
-    
+    // mm, dd, hou, min, sec);
+
     linetime->month = mm;
     linetime->day = dd;
     linetime->hour = hou;
@@ -156,7 +176,8 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
     char sline[4096] = {0};
 
     struct stat sb;
-    if (stat(file, &sb) == -1) {
+    if (stat(file, &sb) == -1)
+    {
         printf("Error: %s\n", strerror(errno));
         return 1;
     }
@@ -166,24 +187,27 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
     long cpos = epos / 2;
 
     FILE *fp = fopen(file, "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         printf("Error: %s\n", strerror(errno));
         return 1;
     }
 
-    if (match_hour == 0 && flag == 0) {
+    if (match_hour == 0 && flag == 0)
+    {
         // 从0点开始
-        if (fgets(sline, sizeof(sline), fp) == NULL) {
+        if (fgets(sline, sizeof(sline), fp) == NULL)
+        {
             printf("Error: %s\n", strerror(errno));
             goto FAIL_EXIT;
         }
         get_line_time(sline, &linetime);
-        if (v > 0) {
+        if (v > 0)
+        {
             printf("  Z: MatchHour[%d] LineHour[%d] spos:0\n", match_hour, linetime.hour);
         }
         retpos->pos = 0;
         copy_linetime(&retpos->linetime, &linetime);
-
 
         goto SUCC_EXIT;
     }
@@ -191,26 +215,32 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
     fseek(fp, cpos, SEEK_SET);
     fgets(sline, sizeof(sline), fp);
 
-    while (fgets(sline, sizeof(sline), fp) != NULL) {
+    while (fgets(sline, sizeof(sline), fp) != NULL)
+    {
         get_line_time(sline, &linetime);
-        if (match_hour <= linetime.hour) { // 在上面
+        if (match_hour <= linetime.hour)
+        { // 在上面
             is_a = 1;
-            if (v > 0) {
+            if (v > 0)
+            {
                 printf("  A: MatchHour[%d] LineHour[%d] spos:%ld cpos:%ld epos:%ld\n", match_hour, linetime.hour, spos, cpos, epos);
             }
 
-            if (match_hour == 0 && flag == 1 && linetime.hour == 0) {
+            if (match_hour == 0 && flag == 1 && linetime.hour == 0)
+            {
                 // 修正: -e 为0的情况
                 copy_linetime(&linetime, &linetime_last);
 
                 break;
             }
 
-            if (is_b) {
+            if (is_b)
+            {
                 epos = cpos;
                 break;
             }
-            if (cpos == epos) {
+            if (cpos == epos)
+            {
                 break;
             }
 
@@ -218,18 +248,22 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
             cpos = epos / 2;
             fseek(fp, cpos, SEEK_SET);
             fgets(sline, sizeof(sline), fp);
-
-        } else if (match_hour > linetime.hour) {    // 在下面
+        }
+        else if (match_hour > linetime.hour)
+        { // 在下面
             is_b = 1;
-            if (v > 0) {
+            if (v > 0)
+            {
                 printf("  B: MatchHour[%d] LineHour[%d] spos:%ld cpos:%ld epos:%ld\n", match_hour, linetime.hour, spos, cpos, epos);
             }
 
-            if (is_a) {
+            if (is_a)
+            {
                 spos = cpos;
                 break;
             }
-            if (spos == cpos) {
+            if (spos == cpos)
+            {
                 break;
             }
 
@@ -240,9 +274,10 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
         }
 
         copy_linetime(&linetime_last, &linetime);
-;
+        ;
     }
-    if (feof(fp)) {
+    if (feof(fp))
+    {
         retpos->pos = epos;
         copy_linetime(&linetime_last, &linetime);
         printf("---EOF---\n");
@@ -252,23 +287,29 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
     copy_linetime(&linetime_last, &linetime);
 
     long step = (float)(epos - spos) * step_c;
-    if (v > 0) {
+    if (v > 0)
+    {
         printf("  Start Step: MatchHour[%d] LineHour[%d] Step:%ld spos:%ld --- epos:%ld\n", match_hour, linetime.hour, step, spos, epos);
     }
 
-    if (step > 0) {
+    if (step > 0)
+    {
         cpos = spos + step;
         fseek(fp, cpos, SEEK_SET);
         fgets(sline, sizeof(sline), fp);
-        while (fgets(sline, sizeof(sline), fp) != NULL) {
+        while (fgets(sline, sizeof(sline), fp) != NULL)
+        {
             get_line_time(sline, &linetime);
-            if (match_hour > linetime.hour) {
+            if (match_hour > linetime.hour)
+            {
                 // 继续向下找
-                if (v > 0) {
-                    printf("  S: [STEP] MatchHour[%d] LineHour[%d]: cpos:%ld\n", match_hour, linetime.hour, cpos);
+                if (v > 0)
+                {
+                    printf("  SU: [STEP] MatchHour[%d] LineHour[%d]: cpos:%ld\n", match_hour, linetime.hour, cpos);
                 }
-                if (cpos > epos) {
-                    printf("  S: [STEP] E cpos(%ld) > epos(%ld), exit.\n", cpos, epos);
+                if (cpos > epos)
+                {
+                    printf("  SU: [STEP] E cpos(%ld) > epos(%ld), exit.\n", cpos, epos);
                     break;
                 }
 
@@ -276,23 +317,32 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
                 cpos = cpos + step;
                 fseek(fp, cpos, SEEK_SET);
                 fgets(sline, sizeof(sline), fp);
-            } else {
-                if (flag == 0) {
-                    if (v > 0) {
-                        printf("  S: [STEP] E MatchHour[%d] LineHour[%d] cpos:%ld\n", match_hour, linetime.hour, cpos);
+            }
+            else
+            {
+                if (flag == 0)
+                {
+                    if (v > 0)
+                    {
+                        printf("  SE: [STEP] E MatchHour[%d] LineHour[%d] cpos:%ld\n", match_hour, linetime.hour, cpos);
                     }
                     cpos = cpos - step;
                     copy_linetime(&linetime, &linetime_last);
                     break;
-                } else {
-                    if (match_hour < linetime.hour) {
-                        if (v > 0) {
-                            printf("  S: [STEP] E MatchHour[%d] LineHour[%d] cpos:%ld\n", match_hour, linetime.hour, cpos);
+                }
+                else
+                {
+                    if (match_hour <= linetime.hour)
+                    {
+                        if (v > 0)
+                        {
+                            printf("  SD: [STEP] E MatchHour[%d] LineHour[%d] cpos:%ld\n", match_hour, linetime.hour, cpos);
                         }
                         break;
                     }
-                    if (v > 0) {
-                        printf("  S: [STEP] MatchHour[%d] LineHour[%d] cpos:%ld\n", match_hour, linetime.hour, cpos);
+                    if (v > 0)
+                    {
+                        printf("  SD: [STEP] MatchHour[%d] LineHour[%d] cpos:%ld\n", match_hour, linetime.hour, cpos);
                     }
                     cpos = cpos + step;
                     fseek(fp, cpos, SEEK_SET);
@@ -300,11 +350,15 @@ int get_pos(char *file, int match_hour, int v, int flag, struct ret_pos_t *retpo
                 }
             }
         }
-
-    } else {
-        if (flag == 0) {
+    }
+    else
+    {
+        if (flag == 0)
+        {
             cpos = spos;
-        } else {
+        }
+        else
+        {
             cpos = epos;
         }
     }
@@ -326,22 +380,28 @@ SUCC_EXIT:
 void grep_str(struct matcher_t *matcher, char *buf, char *substr)
 {
     char *pos = NULL;
-    if (matcher->ignore_case) {
+    if (matcher->ignore_case)
+    {
         pos = strcasestr(buf, substr);
-    } else {
+    }
+    else
+    {
         pos = strstr(buf, substr);
     }
 
-    if (pos != NULL) {
+    if (pos != NULL)
+    {
         // found
-        if (matcher->show_filename) {
+        if (matcher->show_filename)
+        {
             printf("%s: %s%c", matcher->file, buf, eolbyte);
-        } else {
+        }
+        else
+        {
             printf("%s%c", buf, eolbyte);
         }
     }
 }
-
 
 int do_search(struct matcher_t *matcher)
 {
@@ -351,24 +411,31 @@ int do_search(struct matcher_t *matcher)
     // get start pos
     struct ret_pos_t ret_pos_s;
     ret = get_pos(matcher->file, matcher->start_hour, matcher->verbose, 0, &ret_pos_s);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         return 1;
     }
     // printf("Start pos:%ld LineTime:%d:%d\n", ret_pos_s.pos, ret_pos_s.linetime.hour, ret_pos_s.linetime.minute);
 
     // get end pos
     struct ret_pos_t ret_pos_e;
-    if (matcher->end_hour >= 0) {
-        if (matcher->verbose > 0) {
+    if (matcher->end_hour >= 0)
+    {
+        if (matcher->verbose > 0)
+        {
             printf("\n");
         }
         ret = get_pos(matcher->file, matcher->end_hour, matcher->verbose, 1, &ret_pos_e);
-        if (ret != 0) {
+        if (ret != 0)
+        {
             return 1;
         }
-    } else {
+    }
+    else
+    {
         struct stat sb;
-        if (stat(matcher->file, &sb) == -1) {
+        if (stat(matcher->file, &sb) == -1)
+        {
             printf("Error: %s\n", strerror(errno));
             return 1;
         }
@@ -381,20 +448,22 @@ int do_search(struct matcher_t *matcher)
     kscal(ret_pos_e.pos - ret_pos_s.pos, scal_str, sizeof(scal_str));
 
     printf("--------------------------\n");
-    printf("Final: Start:[%ld][%d:%d] --- End:[%ld][%d:%d] Need Scan[%s]\n", 
-        ret_pos_s.pos, ret_pos_s.linetime.hour, ret_pos_s.linetime.minute, 
-        ret_pos_e.pos, ret_pos_e.linetime.hour, ret_pos_e.linetime.minute,
-        scal_str);
+    printf("Final: Start:[%ld][%d:%d] --- End:[%ld][%d:%d] Need Scan[%s]\n",
+           ret_pos_s.pos, ret_pos_s.linetime.hour, ret_pos_s.linetime.minute,
+           ret_pos_e.pos, ret_pos_e.linetime.hour, ret_pos_e.linetime.minute,
+           scal_str);
     printf("--------------------------\n");
     printf("\n");
 
     int fd = open(matcher->file, O_RDONLY);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         printf("Error: open file(%s) fail: %s\n", matcher->file, strerror(errno));
         return 1;
     }
 
-    if (!reset(fd, matcher->file)) {
+    if (!reset(fd, matcher->file))
+    {
         goto ERR_EXIT;
     }
 
@@ -404,16 +473,20 @@ int do_search(struct matcher_t *matcher)
     char *line_end = NULL;
     int len = 0;
     long total_bytes = ret_pos_e.pos - ret_pos_s.pos;
-    while (total_bytes > 0) {
+    while (total_bytes > 0)
+    {
         if (total_bytes < bufalloc)
             bufalloc = total_bytes;
 
-        ssize_t n = read (fd, buffer + len, bufalloc);
-        if (n < 0) {
+        ssize_t n = read(fd, buffer + len, bufalloc);
+        if (n < 0)
+        {
             printf("Error: read file(%s) fail: %s\n", matcher->file, strerror(errno));
             close(fd);
             return 1;
-        } else if (n == 0) {
+        }
+        else if (n == 0)
+        {
             printf("Error: read eof\n");
             close(fd);
             return 0;
@@ -421,28 +494,39 @@ int do_search(struct matcher_t *matcher)
 
         total_bytes -= n;
 
-        if (len == 0) {
+        if (len == 0)
+        {
             // bufbeg 是第一行开始的指针
             line_buf = memchr(buffer, eolbyte, n);
-            if (line_buf) {
+            if (line_buf)
+            {
                 line_buf++;
                 int m = line_buf - buffer;
                 n -= m;
-            } else {
+            }
+            else
+            {
                 printf("Error: buffer alloca is small, is not include \\n at buffer, (you should inc buffer alloca size)\n");
                 goto ERR_EXIT;
             }
-        } else {
+        }
+        else
+        {
             line_buf = buffer;
             n += len;
         }
 
-        if (line_buf) {
-            for (;;) {
+        if (line_buf)
+        {
+            for (;;)
+            {
                 line_end = memchr(line_buf, eolbyte, n);
-                if (line_end) {
+                if (line_end)
+                {
                     *line_end = 0;
-                } else {
+                }
+                else
+                {
                     // 保留 line_buf，不是完整的一行
                     memmove(buffer, line_buf, n);
                     len = n;
@@ -453,9 +537,7 @@ int do_search(struct matcher_t *matcher)
                 n -= (line_end - line_buf + 1);
                 line_buf = line_end + 1;
             }
-
         }
-
     }
 
 ERR_EXIT:
@@ -490,7 +572,8 @@ void usage(char *prog)
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 2)
+    {
         usage(argv[0]);
         return 1;
     }
@@ -505,7 +588,8 @@ int main(int argc, char **argv)
     int i;
     int ch;
     const char *args = "s:e:Sivh";
-    while ((ch = getopt(argc, argv, args)) != -1) {
+    while ((ch = getopt(argc, argv, args)) != -1)
+    {
         switch (ch)
         {
         case 's':
@@ -523,14 +607,15 @@ int main(int argc, char **argv)
         case 'S':
             matcher.show_filename = 1;
             break;
-        
+
         case 'h':
         default:
             usage(argv[0]);
             exit(1);
         }
     }
-    if ((argc - optind) != 2) {
+    if ((argc - optind) != 2)
+    {
         usage(argv[0]);
         return 1;
     }
@@ -547,16 +632,16 @@ int main(int argc, char **argv)
     printf("File: %s\n", matcher.file);
     printf("**************************\n");
 
-    if (matcher.start_hour > matcher.end_hour) {
+    if (matcher.start_hour > matcher.end_hour)
+    {
         printf("Error: -s can't more than -e value\n");
         return 1;
     }
 
     char *ext = strrchr(matcher.file, '.');
-    if (ext && (
-            strcasecmp(ext, ".gz") == 0 ||
-            strcasecmp(ext, ".gzip") == 0)
-            ) {
+    if (ext && (strcasecmp(ext, ".gz") == 0 ||
+                strcasecmp(ext, ".gzip") == 0))
+    {
         printf("Error: file(%s) is gzip file\n", matcher.file);
         printf("  Run: 'gzip -dvc %s > tmp.log'\n", matcher.file);
         return 1;
@@ -569,7 +654,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void copy_linetime(struct line_time_t *dst, struct line_time_t *src) 
+void copy_linetime(struct line_time_t *dst, struct line_time_t *src)
 {
     dst->month = src->month;
     dst->day = src->day;
@@ -577,4 +662,3 @@ void copy_linetime(struct line_time_t *dst, struct line_time_t *src)
     dst->minute = src->minute;
     dst->second = src->second;
 }
-
